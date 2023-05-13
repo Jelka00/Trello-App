22669e743cbe17789f6f3af6b2df04876ece23b1
Nikola Jelic <nj20190121@student.fon.bg.ac.rs>
2023-05-13 16:48:01 +0200

    Aplikacija povezana na MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const tasksRoutes = require("./routes/tasks");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use((req, res, next) => {
@@ -11,6 +12,18 @@ app.use((req, res, next) => {
 
 app.use("/api/tasks", tasksRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Slusam na portu ", process.env.PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //slusanje zahteva
+
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Povezano na db i lusanje zahteva na portu 4000!!!",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
