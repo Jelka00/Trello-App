b7ae685e8298ea0667cd4ea55a9e3ef0c4f3e800
Nikola Jelic <nj20190121@student.fon.bg.ac.rs>
2023-05-14 20:34:52 +0200

    Zavrsni radovi

--- fronted/src/components/TaskDetailsDone.js
+++ fronted/src/components/TaskDetailsDone.js
@@ -1,11 +1,27 @@
-const TaskDetailsInDone = ({ task }) => {
+import { useState } from "react";
+import UpdateTaskPopup from "../components/UpdateTaskPopUp";
+import Modal from "react-modal";
+const TaskDetailsDone = ({ task }) => {
+  const [isEditTaskOpen, setIsEditTaskOpen] = useState(false);
+  const toggleEditTask = () => {
+    setIsEditTaskOpen(!isEditTaskOpen);
+  };
+  function UpdatePopUp() {
+    return (
+      <Modal isOpen={isEditTaskOpen} onRequestClose={toggleEditTask}>
+        <UpdateTaskPopup key={task.id} task={task} />
+      </Modal>
+    );
+  }
+
   if (task.status == "Done") {
     return (
       <div className="task-details">
-        <h4>{task.title}</h4>
+        <h4 onClick={toggleEditTask}>{task.title}</h4>
+        <UpdatePopUp key={task.id} task={task} />
       </div>
     );
   }
 };
 
-export default TaskDetailsInDone;
+export default TaskDetailsDone;
--- fronted/src/components/TaskDetailsInProgress.js
+++ fronted/src/components/TaskDetailsInProgress.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import UpdateTaskPopup from "../components/UpdateTaskPopUp";
+import Modal from "react-modal";
 const TaskDetailsInProgress = ({ task }) => {
+  const [isEditTaskOpen, setIsEditTaskOpen] = useState(false);
+  const toggleEditTask = () => {
+    setIsEditTaskOpen(!isEditTaskOpen);
+  };
+  function UpdatePopUp() {
+    return (
+      <Modal isOpen={isEditTaskOpen} onRequestClose={toggleEditTask}>
+        <UpdateTaskPopup key={task.id} task={task} />
+      </Modal>
+    );
+  }
+
   if (task.status == "In Progress") {
     return (
       <div className="task-details">
-        <h4>{task.title}</h4>
+        <h4 onClick={toggleEditTask}>{task.title}</h4>
+        <UpdatePopUp key={task.id} task={task} />
       </div>
     );
   }
